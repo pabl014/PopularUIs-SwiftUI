2a6214fb35fd35f226b35156def8bd0947269e97
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-04-08 14:26:48 +0200

    Initial Commit

---
+++ PopularUIs-SwiftUI/PopularUIs_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularUIs_SwiftUIApp.swift
+//  PopularUIs-SwiftUI
+//
+//  Created by Paweł Rudnik on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PopularUIs_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
