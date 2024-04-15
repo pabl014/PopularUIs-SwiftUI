e3370d786858a3549b81afe4484de7b28ac6ccab
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-04-15 18:36:04 +0200

    header code refactor

--- PopularUIs-SwiftUI/Spotify/Screens/SpotifyHomeView.swift
+++ PopularUIs-SwiftUI/Spotify/Screens/SpotifyHomeView.swift
@@ -17,45 +17,54 @@ struct SpotifyHomeView: View {
         ZStack {
             Color.spotifyBlack
                 .ignoresSafeArea()
-            HStack {
-                ZStack {
-                    if let currentUser {
-                        ImageLoaderView(urlString: currentUser.image)
-                            .background(.spotifyWhite) // in case an image has some transparency
-                            .clipShape(Circle())
-                            .onTapGesture {
-                                // go to the profile screen
-                            }
-                    }
-                }
-                .frame(width: 30, height: 30)
-                
-                ScrollView(.horizontal) {
-                    HStack(spacing: 8) {
-                        ForEach(Category.allCases, id: \.self) { category in
-                            SpotifyCategoryCell(
-                                title: category.rawValue.capitalized,
-                                isSelected: category == selectedCategory
-                            )
-                            .onTapGesture {
-                                selectedCategory = category
-                            }
-                        }
-                    }
-                }
-                .scrollIndicators(.hidden)
-            }
+            
+            header
         }
         .task {
             await getData()
         }
     }
     
+    
     private func getData() async {
         do {
             currentUser = try await DatabaseHelper().getUsers().first
         } catch {
             // print("Some error occured")
+        }
+    }
+    
+    
+    private var header: some View {
+        
+        HStack(spacing: 0) {
+            ZStack {
+                if let currentUser {
+                    ImageLoaderView(urlString: currentUser.image)
+                        .background(.spotifyWhite) // in case an image has some transparency
+                        .clipShape(Circle())
+                        .onTapGesture {
+                            // go to the profile screen
+                        }
+                }
+            }
+            .frame(width: 35, height: 35)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(Category.allCases, id: \.self) { category in
+                        SpotifyCategoryCell(
+                            title: category.rawValue.capitalized,
+                            isSelected: category == selectedCategory
+                        )
+                        .onTapGesture {
+                            selectedCategory = category
+                        }
+                    }
+                }
+                .padding(.horizontal, 16)
+            }
+            .scrollIndicators(.hidden)
         }
     }
 }
