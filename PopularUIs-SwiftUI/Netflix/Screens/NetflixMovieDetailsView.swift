03d92fd3d10e7296ad6c45a44ac374c4ff8a4ffd
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-05-03 13:48:24 +0200

    Create DetailsView file. Create DetailsHeaderView

---
+++ PopularUIs-SwiftUI/Netflix/Screens/NetflixMovieDetailsView.swift
@@ -0,0 +1,28 @@
+//
+//  NetflixMovieDetailsView.swift
+//  PopularUIs-SwiftUI
+//
+//  Created by Paweł Rudnik on 03/05/2024.
+//
+
+import SwiftUI
+
+struct NetflixMovieDetailsView: View {
+    
+    var product: Product = .mockProduct
+    
+    var body: some View {
+        ZStack {
+            Color.netflixBlack.ignoresSafeArea()
+            Color.netflixDarkGray.opacity(0.3).ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    NetflixMovieDetailsView()
+}
